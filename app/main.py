e13ec7f2ad4db52f06b5c7431e382045ed31dcbf
gdahuks <h.ksiazek.596@studms.ug.edu.pl>
2024-04-25 18:52:42 +0200

    pass stage

--- app/main.py
+++ app/main.py
@@ -1,7 +1,13 @@
 import socket
+from enum import Enum
 
 HOST = "localhost"
 PORT = 4221
+
+
+class Status(Enum):
+    OK = "200 OK"
+    NOT_FOUND = "404 Not Found"
 
 
 class Server:
@@ -19,26 +25,57 @@ class Server:
                 connection, address = server_socket.accept()
                 with connection:
                     while True:
-                        data = connection.recv(1024).decode()
+                        data = connection.recv(4096).decode()
                         if data is None or data == "":
                             break
-                        path = data.split("\r\n")[0].split(" ")[1]
-                        if path.startswith("/echo/"):
-                            message = path[6:]
-                            print(f"Received message: {message}")
-                            connection.sendall(
-                                b"HTTP/1.1 200 OK\r\n"
-                                + b"Content-Type: text/plain\r\n"
-                                + b"Content-Length: "
-                                + str(len(message.encode())).encode()
-                                + b"\r\n\r\n"
-                                + message.encode()
-                            )
-                        elif path == "/":
-                            connection.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
-                        else:
-                            connection.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
+                        Server.handle_request(connection, data)
 
+    @staticmethod
+    def handle_request(connection: socket.socket, data: str):
+        headers, body = Server.get_headers_and_body(data)
+        _, path, _ = Server.get_method_path_protocol(headers)
+        if path.startswith("/echo/"):
+            body_response = path[len("/echo/"):]
+            headers_response = ["Content-Type: text/plain"]
+            Server.response(connection, Status.OK, headers_response, body_response)
+        elif path == "/user-agent":
+            user_agent_header = [header for header in headers if header.startswith("User-Agent: ")][0]
+            body_response = user_agent_header[len("User-Agent: "):]
+            headers_response = ["Content-Type: text/plain"]
+            Server.response(connection, Status.OK, headers_response, body_response)
+        elif path == "/":
+            Server.response(connection, Status.OK, [], "")
+        else:
+            Server.response(connection, Status.NOT_FOUND, [], "")
+
+
+    @staticmethod
+    def get_headers_and_body(data: str) -> (list[str], str):
+        header_body = data.split("\r\n\r\n")
+        if len(header_body) != 2:
+            raise ValueError("Invalid request")
+        header, body = header_body
+        headers = header.split("\r\n")
+        return headers, body
+
+    @staticmethod
+    def get_method_path_protocol(headers: list[str]) -> (str, str, str):
+        if len(headers) == 0:
+            raise ValueError("Invalid request")
+        result = headers[0].split(" ")
+        if len(result) != 3:
+            raise ValueError("Invalid request")
+
+        return result
+
+    @staticmethod
+    def response(connection: socket.socket, status: Status, headers: list[str], body: str):
+        str_builder = [f"HTTP/1.1 {status.value}"]
+        for header in headers:
+            str_builder.append(header)
+        str_builder.append(f"Content-Length: {len(body.encode())}")
+        str_builder.append(f"\r\n{body}")
+        connection.sendall("\r\n".join(str_builder).encode())
 
 def main():
     server = Server(HOST, PORT)
